656572327997a1e60c0ca711996a142137e8b3f2
Chris Beer <cabeer@stanford.edu>
2015-05-22 16:58:58 -0700

    Add spreadsheet validation step

--- app/models/legacy_spreadsheet_parser.rb
+++ app/models/legacy_spreadsheet_parser.rb
@@ -1,4 +1,4 @@
-class LegacySpreadsheetParser
+  class LegacySpreadsheetParser
   attr_reader :io
 
   def initialize(io)
@@ -21,6 +21,39 @@ class LegacySpreadsheetParser
       end
     end
   end
+  
+  # rubocop:disable Metrics/MethodLength
+  def date_ranges
+    @date_ranges ||= begin
+      missing = range.to_a - dates
+
+      return [range] if missing.none?
+
+      last = range.begin
+      ranges = []
+      missing.each_with_object(ranges) do |d|
+        tmp = last
+        last = d + 1
+
+        next if missing.include?(d - 1)
+
+        ranges << (tmp..(d - 1))
+      end
+
+      ranges << (last..range.end) if last < range.end
+
+      ranges
+    end
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  def range
+    @range ||= dates.min..dates.max
+  end
+
+  def dates
+    @dates ||= data.map(&:first).map { |x| Date.parse(x) }
+  end
 
   def locations
     locations_row.reject(&:blank?)
@@ -28,6 +61,10 @@ class LegacySpreadsheetParser
 
   def locations_row
     csv[1]
+  end
+
+  def headers_row
+    csv[2]
   end
 
   def location_index(location)
@@ -70,6 +107,18 @@ class LegacySpreadsheetParser
 
   def data
     csv[3..csv.length]
+  end
+
+  def empty_cells
+    data.each_with_index.map do |row, row_idx|
+      row.each_with_index.map do |cell, col_idx|
+        next unless (locations_row[col_idx] || locations_row[col_idx - 1]).present?
+        next if cell.blank? && (data[row_idx][col_idx - 1]) == 'closed'
+        next if cell == 'closed' || (Time.parse(cell) rescue nil)
+
+        [data[row_idx][0], locations_row[col_idx] || locations_row[col_idx - 1], headers_row[col_idx], cell]
+      end.compact
+    end.compact.flatten(1)
   end
 
   def csv
--- app/models/spreadsheet.rb
+++ app/models/spreadsheet.rb
@@ -4,6 +4,26 @@ class Spreadsheet < ActiveRecord::Base
   validates :attachment, presence: true
 
   def import
-    LegacySpreadsheetParser.new(attachment).process!
+    parser.process!
+  end
+
+  def locations
+    Hash[*parser.locations.map { |l| [l, (Location.find(l) if Location.exists?(l))] }.flatten]
+  end
+
+  def continuous?
+    dates.length == 1
+  end
+
+  def empty_cells
+    parser.empty_cells
+  end
+
+  def dates
+    parser.date_ranges
+  end
+
+  def parser
+    @parser ||= LegacySpreadsheetParser.new(attachment)
   end
 end
