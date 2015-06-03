2d431ed0091e7492509bc7e90455ace858ffcaca
Chris Beer <cabeer@stanford.edu>
2015-06-03 13:56:02 -0700

    Enable squash exception reporting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  include Squash::Ruby::ControllerMethods
+  enable_squash_client
+
   helper_method :current_user
   check_authorization
 
