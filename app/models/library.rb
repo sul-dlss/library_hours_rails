cf2fd3ff3784e1142b0673855a2e1361e624d373
Chris Beer <chris@cbeer.info>
2016-09-26 07:48:58 -0700

    Add link back to library.stanford.edu site for libraries and locations; fixes

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,5 +1,6 @@
 class Library < ActiveRecord::Base
   has_one :node_mapping, as: :mapped
+  delegate :node_id, to: :node_mapping
   has_many :locations
   accepts_nested_attributes_for :locations
   accepts_nested_attributes_for :node_mapping
