12d54af717983c357b639ff0e70b39e3f743f7f4
Jessie Keck <jessie.keck@gmail.com>
2017-03-30 16:03:42 -0700

    Add a sort_key method to the Library class to easily configure a library to be pinned at the top.

---
+++ spec/models/library_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Library, type: :model do
+  describe '#sort_key' do
+    it 'is 0 (which will sort before alpha) when the configured slug matches' do
+      subject.slug = 'green'
+      expect(subject.sort_key).to eq '0'
+    end
+
+    it 'is the name of the library (so they are sorted alphabetically)' do
+      subject.name = 'Library of Fun'
+      expect(subject.sort_key).to eq 'Library of Fun'
+    end
+  end
+end
