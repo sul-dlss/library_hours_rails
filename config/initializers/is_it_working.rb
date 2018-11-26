fd3bedde85943bab3cd29ddc98d1b68eec96bf0c
Chris Beer <cabeer@stanford.edu>
2018-11-26 11:23:38 -0800

    Remove is_it_working

--- config/initializers/is_it_working.rb
+++
@@ -1,6 +0,0 @@
-Rails.configuration.middleware.use(IsItWorking::Handler) do |h|
-  # Check the ActiveRecord database connection without spawning a new thread
-  h.check :active_record, async: false
-
-  h.check :directory, path: File.join(Rails.root, 'tmp', 'uploads'), permission: [:read, :write]
-end
