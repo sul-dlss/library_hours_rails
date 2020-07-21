8b2830002b1a7a7aa85a5c38e8bd2405f062003b
Jessie Keck <jessie.keck@gmail.com>
2020-07-21 14:22:35 -0700

    Add our dev server to the list of allowed hosts in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Since our dev server runs in the rails "development" env, we need to add our host
+  config.hosts << 'sul-library-hours-dev.stanford.edu'
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
