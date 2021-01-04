5649dbdbbc28e1266cc214149fd1bb77ffa1d3c4
Jessie Keck <jessie.keck@gmail.com>
2021-01-04 14:38:22 -0800

    Use ActiveModel::Errors#add instead of appending to the errors array directly.

--- app/models/calendar_range_validator.rb
+++ app/models/calendar_range_validator.rb
@@ -4,6 +4,6 @@ class CalendarRangeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if Calendar.valid_range?(value) || value.blank?
 
-    record.errors[attribute] << "#{value} is not a time range"
+    record.errors.add(attribute, "#{value} is not a time range")
   end
 end
