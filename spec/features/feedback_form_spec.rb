5fe8fa0d092d6ec39e505c02d613b5e3ad950158
Nick Budak <budak@stanford.edu>
2022-01-27 14:41:38 -0800

    Change form spec matchers to look for classes

--- spec/features/feedback_form_spec.rb
+++ spec/features/feedback_form_spec.rb
@@ -3,40 +3,43 @@
 require 'rails_helper'
 
 RSpec.describe 'Feedback form', type: :feature, js: true do
+  before do
+    visit root_path
+  end
+
+  it 'is initially collapsed' do
+    expect(page).not_to have_css('#feedback-form.in')
+  end
+
+  it 'can be expanded' do
+    click_link 'Feedback', match: :first
+    expect(page).to have_css('#feedback-form.in')
+  end
+
+  it 'can be collapsed' do
+    click_link 'Feedback', match: :first
+    find('button.cancel-link').click
+    expect(page).not_to have_css('#feedback-form.in')
+  end
 
   context 'when not logged in' do
-    before(:each) do
-      visit root_path
-    end
-
-    it 'is initially hidden' do
-      expect(page).to have_css('#feedback-form', visible: false)
-    end
-
-    it 'can be expanded' do
+    it 'shows the reCAPTCHA challenge' do
       click_link 'Feedback', match: :first
-      expect(page).to have_css('#feedback-form', visible: true)
-    end
-
-    it 'can be collapsed' do
-      click_link 'Feedback', match: :first
-      find('button.cancel-link').click
-      expect(page).to have_css('#feedback-form', visible: false)
-    end
-
-    it 'reCAPTCHA challenge is present' do
-      click_link 'Feedback', match: :first
-      expect(page).to have_css('.library-hours-captcha', visible: false)
+      expect(page).to have_css('.library-hours-captcha')
     end
   end
 
   context 'when logged in' do
-    before(:each) do
+    before do
       stub_current_user(build(:superadmin_user))
-      visit root_path
     end
 
-    xit 'shows a success message when submitted' do
+    it 'does not show the reCAPTCHA challenge' do
+      click_link 'Feedback', match: :first
+      expect(page).not_to have_css('.library-hours-captcha')
+    end
+
+    it 'shows a success message when submitted' do
       click_link 'Feedback', match: :first
       within 'form.feedback-form' do
         fill_in('message', with: 'This is only a test')
@@ -45,12 +48,6 @@ RSpec.describe 'Feedback form', type: :feature, js: true do
         click_button 'Send'
       end
       expect(page).to have_css('.alert-success', text: 'Thank you! Your feedback has been sent.')
-    end
-
-    it 'reCAPTCHA challenge is not present' do
-      visit root_path
-      click_link 'Feedback', match: :first
-      expect(page).not_to have_css('.library-hours-captcha', visible: false)
     end
   end
 end
