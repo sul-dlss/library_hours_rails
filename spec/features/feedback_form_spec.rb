80f6a1f64bd73e1dd420b95eabb623b7d8371ca8
Nick Budak <budak@stanford.edu>
2022-01-27 11:40:18 -0800

    WIP feedback form test refactor

--- spec/features/feedback_form_spec.rb
+++ spec/features/feedback_form_spec.rb
@@ -2,58 +2,49 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Feedback form', type: :feature do
+RSpec.describe 'Feedback form', type: :feature, js: true do
+
+  it 'is initially hidden' do
+    visit root_path
+    expect(page).to have_css('form.feedback-form', visible: :hidden)
+  end
+
+  it 'can be expanded' do
+    visit root_path
+    click_link 'Feedback', match: :first
+    expect(page).to have_css('form.feedback-form', visible: :visible)
+  end
+
+  it 'can be collapsed' do
+    visit root_path
+    click_link 'Feedback', match: :first
+    find('button.cancel-link').click
+    expect(page).to have_css('form.feedback-form', visible: :hidden)
+  end
+
+  it 'shows a success message when submitted' do
+    visit root_path
+    click_link 'Feedback', match: :first
+    within 'form.feedback-form' do
+      fill_in('message', with: 'This is only a test')
+      fill_in('name', with: 'Ronald McDonald')
+      fill_in('to', with: 'test@kittenz.eu')
+      click_button 'Send'
+    end
+    expect(page).to have_css('div.alert-success', text: 'Thank you! Your feedback has been sent.')
+  end
+
   context 'when not logged in' do
     it 'reCAPTCHA challenge is present' do
-      visit feedback_path
+      visit root_path
       expect(page).to have_css '.library-hours-captcha'
     end
   end
 
-  context 'with js', js: true do
-    before do
-      visit root_path
-    end
-
-    it 'feedback form should be hidden' do
-      expect(page).not_to have_css('#feedback-form', visible: :visible)
-    end
-
-    it 'feedback form should be shown filled out and submitted' do
-      click_link 'Feedback'
-      expect(page).to have_css('#feedback-form', visible: :visible)
-      expect(page).to have_css('button', text: 'Cancel')
-      within 'form.feedback-form' do
-        fill_in('message', with: 'This is only a test')
-        fill_in('name', with: 'Ronald McDonald')
-        fill_in('to', with: 'test@kittenz.eu')
-        click_button 'Send'
-      end
-      expect(page).to have_css('div.alert-success', text: 'Thank you! Your feedback has been sent.')
-    end
-  end
-
-  context 'without js' do
-    before do
-      visit root_path
-    end
-
-    it 'reCAPTCHA challenge is present' do
+  context 'when logged in' do
+    it 'reCAPTCHA challenge is not present' do
       visit feedback_path
       expect(page).not_to have_css '.library-hours-captcha'
-    end
-
-    it 'feedback form should be shown filled out and submitted' do
-      click_link 'Feedback', match: :first
-      expect(page).to have_css('#feedback-form', visible: :visible)
-      expect(page).to have_css('a', text: 'Cancel')
-      within 'form.feedback-form' do
-        fill_in('message', with: 'This is only a test')
-        fill_in('name', with: 'Ronald McDonald')
-        fill_in('to', with: 'test@kittenz.eu')
-        click_button 'Send'
-      end
-      expect(page).to have_css('div.alert-success', text: 'Thank you! Your feedback has been sent.')
     end
   end
 end
