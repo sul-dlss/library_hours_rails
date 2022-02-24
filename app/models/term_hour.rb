530b0ec28e9f9a7d0722dd9ffd0157e2a10068a2
Chris Beer <chris@cbeer.info>
2022-02-24 15:15:23 -0800

    Strip whitespace from term hour data

--- app/models/term_hour.rb
+++ app/models/term_hour.rb
@@ -11,4 +11,32 @@ class TermHour < ApplicationRecord
   delegate :library, to: :location
 
   validates :sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, calendar_range: true
+
+  def sunday=(val)
+    super(val.strip)
+  end
+
+  def monday=(val)
+    super(val.strip)
+  end
+
+  def tuesday=(val)
+    super(val.strip)
+  end
+
+  def wednesday=(val)
+    super(val.strip)
+  end
+
+  def thursday=(val)
+    super(val.strip)
+  end
+
+  def friday=(val)
+    super(val.strip)
+  end
+
+  def saturday=(val)
+    super(val.strip)
+  end
 end
