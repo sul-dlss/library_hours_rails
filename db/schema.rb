260b71b64331e965625787615f3299eb1c94df29
Chris Beer <chris@cbeer.info>
2022-06-23 08:17:57 -0700

    Update to Rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_223021) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_06_23_151711) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.integer "record_id", null: false
     t.integer "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -28,8 +27,8 @@ ActiveRecord::Schema.define(version: 2021_01_04_223021) do
     t.string "content_type"
     t.text "metadata"
     t.integer "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
+    t.string "checksum"
+    t.datetime "created_at", precision: nil, null: false
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
@@ -41,14 +40,14 @@ ActiveRecord::Schema.define(version: 2021_01_04_223021) do
   end
 
   create_table "calendars", force: :cascade do |t|
-    t.datetime "dtstart"
-    t.datetime "dtend"
+    t.datetime "dtstart", precision: nil
+    t.datetime "dtend", precision: nil
     t.integer "location_id"
     t.string "summary"
     t.text "description"
     t.boolean "closed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["location_id"], name: "index_calendars_on_location_id"
   end
 
@@ -57,7 +56,7 @@ ActiveRecord::Schema.define(version: 2021_01_04_223021) do
     t.integer "sluggable_id", null: false
     t.string "sluggable_type", limit: 50
     t.string "scope"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
     t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
     t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
@@ -67,8 +66,8 @@ ActiveRecord::Schema.define(version: 2021_01_04_223021) do
   create_table "libraries", force: :cascade do |t|
     t.string "name"
     t.string "slug"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "locations", force: :cascade do |t|
@@ -76,8 +75,8 @@ ActiveRecord::Schema.define(version: 2021_01_04_223021) do
     t.string "slug"
     t.boolean "keeps_hours", default: false
     t.integer "library_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["library_id"], name: "index_locations_on_library_id"
   end
 
@@ -85,8 +84,8 @@ ActiveRecord::Schema.define(version: 2021_01_04_223021) do
     t.integer "node_id"
     t.string "mapped_type"
     t.integer "mapped_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "spreadsheets", force: :cascade do |t|
@@ -95,27 +94,27 @@ ActiveRecord::Schema.define(version: 2021_01_04_223021) do
     t.string "attachment_content_type"
     t.integer "attachment_size"
     t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "term_hours", force: :cascade do |t|
     t.integer "term_id"
     t.integer "location_id"
     t.text "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["location_id"], name: "index_term_hours_on_location_id"
     t.index ["term_id"], name: "index_term_hours_on_term_id"
   end
 
   create_table "terms", force: :cascade do |t|
-    t.datetime "dtstart"
-    t.datetime "dtend"
+    t.datetime "dtstart", precision: nil
+    t.datetime "dtend", precision: nil
     t.string "name"
     t.boolean "holiday", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
