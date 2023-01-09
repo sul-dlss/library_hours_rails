7ee416810ad76d6e98e8d2f87e3a46b0022272e1
Justin Coyne <jcoyne@justincoyne.com>
2023-01-09 15:01:28 -0600

    Remove unused jquery nested form plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require jquery_nested_form
 //= require popper
 //= require bootstrap-sprockets
 //= require bootstrap-editable
