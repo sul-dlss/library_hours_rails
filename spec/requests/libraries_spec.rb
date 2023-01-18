fd038cc7f10af80b89af03b4f2f524cc9d025a24
Chris Beer <chris@cbeer.info>
2023-01-18 13:23:03 -0800

    Display the primary location hours on the library

--- spec/requests/libraries_spec.rb
+++ spec/requests/libraries_spec.rb
@@ -13,4 +13,41 @@ RSpec.describe 'Libraries', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET /libraries.json' do
+    let!(:library) { create(:library, slug: 'green') }
+    let!(:location) { create(:location, library: library, primary: true, keeps_hours: true) }
+
+    before do
+      location.calendars.create(dtstart: Time.zone.now.beginning_of_day, dtend: Time.zone.now.end_of_day, summary: 'Open')
+    end
+
+    it 'has a primary location attribute' do
+      get libraries_path(format: :json)
+      expect(response).to have_http_status(200)
+
+      data = JSON.parse(response.body).with_indifferent_access
+
+      expect(data.dig(:data, 0, :attributes, :primary_location)).to eq("green/#{location.slug}")
+    end
+
+    it 'elevates the primary location hours to the library' do
+      get libraries_path(format: :json)
+      expect(response).to have_http_status(200)
+
+      data = JSON.parse(response.body).with_indifferent_access
+
+      expect(data.dig(:data, 0, :attributes, :hours)).to include(hash_including(type: 'Open'))
+    end
+
+    it 'includes the library locations' do
+      get libraries_path(format: :json)
+      expect(response).to have_http_status(200)
+
+      data = JSON.parse(response.body).with_indifferent_access
+
+      expect(data.dig(:included, 0, :id)).to eq(data.dig(:data, 0, :attributes, :primary_location))
+      expect(data.dig(:included, 0, :attributes)).to include(name: location.name, primary: true)
+    end
+  end
 end
