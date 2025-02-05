734e1bbfb08d67413a49f84d1386aabd953c237f
Chris Beer <cabeer@stanford.edu>
2025-02-05 12:08:54 -0800

    Fix the bootstrap javascript loading.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
 import * as Popper from "@popperjs/core"
-import "bootstrap"
+import bootstrap from "bootstrap"
 import "@hotwired/turbo-rails"
 
-window.addEventListener("load", function() {
-  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+['load', 'turbo:load'].forEach(event => {
+  window.addEventListener(event, function() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"][title]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  });
 });
