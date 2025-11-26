420dee039e74939db07db58f21bead3507163226
Justin Coyne <jcoyne@justincoyne.com>
2025-11-26 10:16:25 -0600

    Refactor javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
-import * as Popper from "@popperjs/core"
+import "@popperjs/core"
 import bootstrap from "bootstrap"
 import "@hotwired/turbo-rails"
 
-['load', 'turbo:load'].forEach(event => {
-  window.addEventListener(event, function() {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"][data-bs-title]:not([data-bs-title=""]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+;["load", "turbo:load"].forEach(event => {
+  window.addEventListener(event, function () {
+    const tooltipTriggerList = document.querySelectorAll(
+      '[data-bs-toggle="tooltip"][data-bs-title]:not([data-bs-title=""]'
+    )
+    tooltipTriggerList.forEach(
+      tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl)
+    )
 
-    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-  });
-});
+    const popoverTriggerList = document.querySelectorAll(
+      '[data-bs-toggle="popover"]'
+    )
+    popoverTriggerList.forEach(
+      popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl)
+    )
+  })
+})
